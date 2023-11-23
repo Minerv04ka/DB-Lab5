018b39a3d7e75a5cb39fc0d88e7c9ef48943c84d
Minerv04ka <115411598+Minerv04ka@users.noreply.github.com>
2023-11-23 03:22:52 +0200

    lab5.sql

--- lab5.sql
+++ lab5.sql
@@ -2,10 +2,10 @@
 -- version 5.1.2
 -- https://www.phpmyadmin.net/
 --
--- Хост: localhost:3306
--- Час створення: Лис 20 2023 р., 10:24
--- Версія сервера: 5.7.24
--- Версія PHP: 8.0.1
+-- Host: localhost:3306
+-- Generation Time: Nov 23, 2023 at 01:22 AM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База даних: `lab5`
+-- Database: `lab5`
 --
 
 -- --------------------------------------------------------
 
 --
--- Структура таблиці `customers`
+-- Table structure for table `customers`
 --
 
 CREATE TABLE `customers` (
@@ -35,10 +35,17 @@ CREATE TABLE `customers` (
   `CustomerPhone` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`CustomerID`, `CustomerName`, `CustomerSurname`, `CustomerEmail`, `CustomerPhone`) VALUES
+(1, 'Sasha', 'Zhuk', 'oleksandr.zhuk.2000@gmail.com', '321321321');
+
 -- --------------------------------------------------------
 
 --
--- Структура таблиці `distributors`
+-- Table structure for table `distributors`
 --
 
 CREATE TABLE `distributors` (
@@ -48,10 +55,17 @@ CREATE TABLE `distributors` (
   `DistributorPhone` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `distributors`
+--
+
+INSERT INTO `distributors` (`DistributorID`, `DistributorName`, `DistributorEmail`, `DistributorPhone`) VALUES
+(1, 'Vlad', 'gipnoboy@gmail.com', '111222333');
+
 -- --------------------------------------------------------
 
 --
--- Структура таблиці `mechanics`
+-- Table structure for table `mechanics`
 --
 
 CREATE TABLE `mechanics` (
@@ -62,25 +76,39 @@ CREATE TABLE `mechanics` (
   `MechanicsPhone` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `mechanics`
+--
+
+INSERT INTO `mechanics` (`MechanicsID`, `MechanicsName`, `MechanicsSurname`, `MechanicsEmail`, `MechanicsPhone`) VALUES
+(1, 'Maks', 'Skamer', 'maksskam228@gmail.com', '333222111');
+
 -- --------------------------------------------------------
 
 --
--- Структура таблиці `orders`
+-- Table structure for table `orders`
 --
 
 CREATE TABLE `orders` (
   `OrderID` int(11) NOT NULL,
   `DistributorID` int(11) DEFAULT NULL,
-  `MechanicsID` int(11) DEFAULT NULL,
+  `CustomerID` int(11) DEFAULT NULL,
   `Spare_partsID` int(11) DEFAULT NULL,
   `Quantity` int(11) DEFAULT NULL,
   `Total` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`OrderID`, `DistributorID`, `CustomerID`, `Spare_partsID`, `Quantity`, `Total`) VALUES
+(1, 1, 1, 1, 1, 2);
+
 -- --------------------------------------------------------
 
 --
--- Структура таблиці `receipts`
+-- Table structure for table `receipts`
 --
 
 CREATE TABLE `receipts` (
@@ -95,60 +123,75 @@ CREATE TABLE `receipts` (
 -- --------------------------------------------------------
 
 --
--- Структура таблиці `spare_parts`
+-- Table structure for table `spare_parts`
 --
 
 CREATE TABLE `spare_parts` (
   `Spare_partsID` int(11) NOT NULL,
-  `Spare_partsName` int(11) DEFAULT NULL,
+  `Spare_partsName` varchar(99) DEFAULT NULL,
   `Spare_partsPrice` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `spare_parts`
+--
+
+INSERT INTO `spare_parts` (`Spare_partsID`, `Spare_partsName`, `Spare_partsPrice`) VALUES
+(1, 'Фара', 1000),
+(2, 'Двигун', 10000);
 
 -- --------------------------------------------------------
 
 --
--- Структура таблиці `storage_`
+-- Table structure for table `storage_`
 --
 
 CREATE TABLE `storage_` (
-  `PlaceInStorage` int(11) NOT NULL,
+  `PlaceInStorage` varchar(100) NOT NULL,
   `Spare_partsID` int(11) DEFAULT NULL,
   `Quantity` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Індекси збережених таблиць
+-- Dumping data for table `storage_`
+--
+
+INSERT INTO `storage_` (`PlaceInStorage`, `Spare_partsID`, `Quantity`) VALUES
+('Склад', 2, 2);
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Індекси таблиці `customers`
+-- Indexes for table `customers`
 --
 ALTER TABLE `customers`
   ADD PRIMARY KEY (`CustomerID`);
 
 --
--- Індекси таблиці `distributors`
+-- Indexes for table `distributors`
 --
 ALTER TABLE `distributors`
   ADD PRIMARY KEY (`DistributorID`);
 
 --
--- Індекси таблиці `mechanics`
+-- Indexes for table `mechanics`
 --
 ALTER TABLE `mechanics`
   ADD PRIMARY KEY (`MechanicsID`);
 
 --
--- Індекси таблиці `orders`
+-- Indexes for table `orders`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`OrderID`),
   ADD KEY `DistributorID` (`DistributorID`),
-  ADD KEY `MechanicsID` (`MechanicsID`),
+  ADD KEY `MechanicsID` (`CustomerID`),
   ADD KEY `Spare_partsID` (`Spare_partsID`);
 
 --
--- Індекси таблиці `receipts`
+-- Indexes for table `receipts`
 --
 ALTER TABLE `receipts`
   ADD PRIMARY KEY (`ReceiptID`),
@@ -157,32 +200,32 @@ ALTER TABLE `receipts`
   ADD KEY `Spare_partsID` (`Spare_partsID`);
 
 --
--- Індекси таблиці `spare_parts`
+-- Indexes for table `spare_parts`
 --
 ALTER TABLE `spare_parts`
   ADD PRIMARY KEY (`Spare_partsID`);
 
 --
--- Індекси таблиці `storage_`
+-- Indexes for table `storage_`
 --
 ALTER TABLE `storage_`
   ADD PRIMARY KEY (`PlaceInStorage`),
   ADD KEY `Spare_partsID` (`Spare_partsID`);
 
 --
--- Обмеження зовнішнього ключа збережених таблиць
+-- Constraints for dumped tables
 --
 
 --
--- Обмеження зовнішнього ключа таблиці `orders`
+-- Constraints for table `orders`
 --
 ALTER TABLE `orders`
   ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`DistributorID`) REFERENCES `distributors` (`DistributorID`),
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`MechanicsID`) REFERENCES `mechanics` (`MechanicsID`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`CustomerID`) REFERENCES `mechanics` (`MechanicsID`),
   ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`Spare_partsID`) REFERENCES `spare_parts` (`Spare_partsID`);
 
 --
--- Обмеження зовнішнього ключа таблиці `receipts`
+-- Constraints for table `receipts`
 --
 ALTER TABLE `receipts`
   ADD CONSTRAINT `receipts_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`CustomerID`),
@@ -190,7 +233,7 @@ ALTER TABLE `receipts`
   ADD CONSTRAINT `receipts_ibfk_3` FOREIGN KEY (`Spare_partsID`) REFERENCES `spare_parts` (`Spare_partsID`);
 
 --
--- Обмеження зовнішнього ключа таблиці `storage_`
+-- Constraints for table `storage_`
 --
 ALTER TABLE `storage_`
   ADD CONSTRAINT `storage__ibfk_1` FOREIGN KEY (`Spare_partsID`) REFERENCES `spare_parts` (`Spare_partsID`);
